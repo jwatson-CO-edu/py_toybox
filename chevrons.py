a472542b8006eb216c956bc45eab04558e03819b
jwatson_utah_edu <james.r.watson@utah.edu>
2016-11-21 10:18:02 -0700

    Added minecraft

--- chevrons.py
+++ chevrons.py
@@ -1,7 +1,12 @@
+# chevrons.py
+# Print some random, angular glyphs
+
+# TODO: Make the dicerolls tunable
+
 import random
 
 for i in xrange(100):
     row = ""
     for j in xrange(100):
-        row += random.choice( ['\\','/','-','|','_'] )
+        row += random.choice( ['\\','/','-','|','_',' ',' '] )
     print row
