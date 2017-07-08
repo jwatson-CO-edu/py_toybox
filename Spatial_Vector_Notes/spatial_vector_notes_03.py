8c71ed8c5a8cca70b2f8688b14d15ed8be2b52c3
jrwatson_utah_edu <james.r.watson@utah.edu>
2017-07-08 07:01:58 -0600

    Starting FK and IK

--- Spatial_Vector_Notes/spatial_vector_notes_03.py
+++ Spatial_Vector_Notes/spatial_vector_notes_03.py
@@ -28,9 +28,19 @@ import numpy as np
 # ~ Local ~
 localPaths = [ os.path.join( "C:" , os.sep , "Users" , "jwatson" , "Documents" , "Python Scripts" ) ] # List of paths to your custom modules
 add_valid_to_path( localPaths )
-from XOS_util import concat_arr , eq
+from XOS_util import concat_arr , eq , enumerate_rev , np_dot
 
 # == End Init ==============================================================================================================================
+
+# == Utility Functions ==
+
+def left_divide( A , b ):
+    """ Least-squares solution to an under to over-determined linear system  
+        x = left_divide( A , b ) is the solution to dot(A,x) = b , and equivalent to MATLAB A\b """
+    x , resid , rank , s = np.linalg.lstsq( A , b )
+    return x
+
+# == End Utility ==
 
 # == 3D Vector Arithmetic ==
 
@@ -84,26 +94,37 @@ def cross_forc( motionVec , forceVec ):
     return concat_arr( np.cross( motionVec[:3] , forceVec[:3] ) + np.cross( motionVec[3:] , forceVec[3:] ) , 
                        np.cross( motionVec[:3] , forceVec[3:] ) )
 
-def cross_motn_matx( motionVec ): # Acts on a motion vector --> produces a motion vector
+def cross_motn_matx( motionVec ): # (Featherstone: crm) # Acts on a motion vector --> produces a motion vector
     """ Return the matrix for the equivalent cross operation: [motionVec_cross][v] = cross( motionVec , v ) """
     return np.vstack( ( np.hstack( ( skew_sym_cross( motionVec[:3] ) , np.zeros( (3,3) )               ) ) , 
                         np.hstack( ( skew_sym_cross( motionVec[3:] ) , skew_sym_cross( motionVec[:3] ) ) ) ) ) # 6x6
     
-def cross_forc_matx( forceVec ): # Acts on a force vector --> produces a force vector
+def cross_forc_matx( forceVec ): # (Featherstone: crf) # Acts on a force vector --> produces a force vector
     """ Return the matrix for the equivalent cross operation: [forceVec_cross][v] = cross_star( forceVec , v ) """
     return np.vstack( ( np.hstack( ( skew_sym_cross( forceVec[:3] ) , skew_sym_cross( forceVec[3:] ) ) ) , 
                         np.hstack( ( np.zeros( (3,3) )              , skew_sym_cross( forceVec[:3] ) ) ) ) ) # 6x6
     
 
+def XtoV( X ):
+    """ Calculate the small-magnitude motion vector from the transform for a small change in coordinates """
+    return np.multiply( 0.5 , 
+                        [ X[1][2] - X[2][1]  ,  X[2][0] - X[0][2]  ,  X[0][1] - X[1][0] , 
+                          X[4][2] - X[5][1]  ,  X[5][0] - X[3][2]  ,  X[3][1] - X[4][0] ] )
+    """ Suppose A and B are two Cartesian frames ( and also the Plucker coordinate systems defined by those frames ) 
+    If A and B are close together , then 'XtoV' approximates the the velocity vector that would move frame A to coincide with B after one
+    timestep. This is invariant to 'X' and therefore would be the same for A and B. """
+
 # == End Plucker ==
 
 
-# == Spatial Dynamics ==
+# == Robot Model ==
 
-# TODO: Write the function outline and fill in the details as the article progresses
+# TODO: Fill in the details of the inverse dynamics function as the article progresses
+
+# = Model Classes & Functions =
 
 class LinkSpatial:
-    """ Represents a rigid link using spatial coordinates """
+    """ Represents a rigid link and the associated joint using spatial coordinates """
     def __init__( self , pName , pPitch ):
         """ Rigid link struct """
         self.name = pName # - String that uniquely identifies the link
@@ -113,13 +134,26 @@ class LinkSpatial:
         self.linkIndex = 0 #- Index of link in q 
         self.xform = None # - Plucker coordinate transform that describes the relative position of this joint in the parent frame
         self.I = None # ----- Spatial inertia for this link
+        self.q = 0 # -------- Joint configuration ---> These used for caching and for preserving state across timesteps
+        self.qDot = 0 # ----- Joint Velocity        /
+        self.qDotDot = 0 # -- Joint Acceleration  _/
+        self.v = 0 # -------- Link Spatial Velocity     ---> These are the result of dynamics calculations
+        self.a = 0 # -------- Link Spatial Acceleration   /
+        self.f = 0 # -------- Joint Spatial Force        /
+        self.tau = 0 # ------ Torque about this joint  _/
+        
+    def calc_link_I( self , mass , COM , I_COM ):
+        """ Calculate and set the link rotational inertial about the joint """
+        cX = cross_motn_matx( COM )
+        self.I = np.vstack( ( np.hstack( ( I_COM - np.multiply( mass , np.dot( cX , cX ) ) , np.multiply( mass , cX )        ) ) , 
+                              np.hstack( ( np.multiply( -mass , cX )                       , np.multiply( mass , np.eye(3) ) ) ) ) )
 
 class LinkModel(object):
     """ Represents a serial-link manipulator composed of rigid links and defined with spatial vectors """
     
-    def __init__( self , pN ):
+    def __init__( self ):
         """ Create a model with an empty list of links """
-        self.N = pN; # ------------------------------ Number of links in the mechanism
+        self.N = 0 # -------------------------------- Number of links in the mechanism
         self.links = []; # -------------------------- Links of the model , stored in the order of q
         self.names = set([]) # ---------------------- Set of all link names in the model
         self.gravity = [ 0 , 0 , 0 , 0 , 0 , 9.81 ] # Default direction of gravity as a spatial vector
@@ -134,6 +168,8 @@ class LinkModel(object):
         
     def add_and_attach( self , link , parentName = None ):
         """ Add the link to the model , then  """
+        link.linkIndex = self.N
+        self.N += 1
         self.names.add( link.name )
         self.links.append( link )
         if parentName in self.names: # If there was a parent link specified and such a link has been added to the model
@@ -142,6 +178,12 @@ class LinkModel(object):
             parent.children.append( link ) # Add the child reference
             
     # NOTE: Not implementing the ability to remove links at this time
+    
+    def set_q( self , q ):
+        """ Set the configuration of the robot """
+        assert( len( q ) == len( self.links ) , "LinkModel.set_q: 'len(q)' must equal the number of links!" )
+        for lnkDex , link in enumerate( self.links ):
+            link.q = q[ lnkDex ]
         
 def joint_xform( pitch , q ): # Featherstone: jcalc
     """ Return the joint transform and subspace matrix for a joint with 'pitch' and joint variable 'q' """
@@ -156,18 +198,89 @@ def joint_xform( pitch , q ): # Featherstone: jcalc
         s_i = [ 0 , 0 , 1 , 0 , 0 , pitch ]
     return XJ , s_i
 
-def inverse_dynamics( model , q , qDot , qDotDot ):
-    """ Compute the inverse dynamics from the model , positions , speeds , and accelerations """
-    for lnkDex , link in enumerate( model.links ): # For every link in the model , do
-        [ XJ , s_i ] = joint_xform( link.pitch , q[ lnkDex ] ) # (FS: 'jcalc') calc the joint transformation and freedom (selection) matrix
-        vJ = np.dot( s_i * qDot[ lnkDex ] ) # Calc joint velocity
-        Xup = np.dot( XJ , link.xform ) # Transform the link coordinates into the world frame
-        if not link.parent:
-            
-        
-    
+# = End Classes =
 
-# == End Dynamics == 
+# = Spatial Dynamics =
+
+def inverse_dynamics( model ): # , q , qDot , qDotDot ):
+    """ Compute the inverse dynamics from the model with the Recursive Newton-Euler Algo """ 
+    # NOTE: This function assumes that 'model' has 'q' , 'qDot' , 'qDotDot' set for joints
+    # ~ Forward Pass ~
+    for lnkDex , link in enumerate( model.links ): # For every link in the model , do
+        [ XJ , s_i ] = joint_xform( link.pitch , link.q ) # (FS: 'jcalc') calc the joint transformation and freedom (selection) matrix
+        vJ = np.dot( s_i , link.qDot ) # Calc joint velocity
+        Xup = np.dot( XJ , link.xform ) # Transform the link coordinates into the world frame
+        if not link.parent: # if there is no parent link
+            link.v = vJ
+            link.a = np.dot( Xup , model.gravity ) + np.dot( s_i , link.qDotDot )
+        else: # else link is child of another link # NOTE : This will cause problems unless the links are added in levels root to leaf
+            link.v = np.dot( Xup , link.parent.v ) + vJ
+            link.a = np.dot( Xup , link.parent.a ) + np.dot( s_i , link.qDotDot ) + np.dot( cross_motn_matx( link.v ) , vJ )
+        link.f = np.dot( link.I , link.a ) + np.dot( np.dot( cross_forc_matx( link.v , link.I ) ) , 
+                                                     link.v )
+    # ~ Backward Pass ~ # NOTE : This will cause problems unless the links are added in levels root to leaf
+    for lnkDex , link in enumerate_rev( model.links ): # For every link in the model (reverse order) , do
+        link.tau = np.dot( np.transpose( s_i ) , link.f )
+        if link.parent:
+            link.parent.f += np.dot( np.transpose( Xup ) , link.f )
+    
+# = End Dynamics =
+
+# = Forward Kinematics =
+
+def FK( model , bodyIndex , q ): # ( Featherstone: bodypos )
+    """ Compute the pose for a given config 'q' """
+    # NOTE: This function does not depend on the presently stored q state , returned pose depends only on given 'q'
+    X = np.eye(6) # Init pose frame
+    body = model.links[ bodyIndex ] # Fetch the link by index
+    while body: 
+        [ XJ , s_i ] = joint_xform( body.pitch , q[ bodyIndex ] )
+        X = np_dot( X , XJ , body.xform )
+        body = body.parent # This will become 'None' after the root link has been processed
+        
+def jacobn_manip( model , bodyIndex , q ): # ( Featherstone: bodyJac )
+    """ Compute the manipulator jacobian up to the specified link in the tree """
+    # NOTE: This function does not depend on the presently stored q state , returned pose depends only on given 'q'
+    e = np.zeros( model.N )
+    body = model.links[ bodyIndex ] # Fetch the link by index
+    while body:
+        e[ body.linkIndex ] = 1
+        body = body.parent # This will become 'None' after the root link has been processed
+    J = np.zeros( ( 6 , model.N ) ) # Matrix for the Jacobian
+    Xa = np.zeros( model.N ) # An array of transforms
+    for lnkDex , link in enumerate( model.links ): 
+        if e[ lnkDex ]:
+            [ XJ , s_i ] = joint_xform( link.pitch , q[ lnkDex ] )
+            Xa[ lnkDex ] = np.dot( XJ , link.xform )
+            if link.parent:
+                Xa[ lnkDex ] = np.dot( Xa[ lnkDex ] , Xa[ link.parent.linkIndex ] )
+            x , resid , rank , s = np.linalg.lstsq( Xa[ lnkDex ] , s_i )
+            J[ : , lnkDex ] = x
+    return J
+
+# = End FK =
+
+# = Inverse Kinematcis =
+
+# NOTE: This does not check for singularities or infeasible poses and is not a serious IK solution
+
+def IK_pos( model , body , Xd , q0 , criterion = 1e-10 ): 
+    """ Perform iterative Jacobian IK for pose 'Xd' starting from configuration 'q0' """ # TODO: Consider implementing an iteration limit
+    q = q0 # Init q to the specified starting state
+    dPos = np.ones( 6 ) # Init position difference # This is very likely to not define the desired pose
+    while np.linalg.norm( dPos ) > criterion: # While the body pose is not close enough to the desired body pose
+        X = FK( model , body.linkIndex , q ) # Calc X_{d->b}
+        J = jacobn_manip( model , body.linkIndex , q ) # Calc the manipulator Jacobian for the link
+        Jm = np.dot( X , J ) # Calc J_{b->b} , Get the jacobian in the body's own frame
+        dPos = XtoV( left_divide( Xd , X ) ) # Calc a difference vector between the present and the desired pose
+        Jm_T = np.transpose( Jm )
+        dq = np.dot( Jm_T , ( left_divide( np.dot( Jm , Jm_T ) , dPos ) ) ) # qDelta = dot( J_pinv_{b->b} , pDelta ) # pseudo-inverse step
+        q += dq # Increment q
+    return q
+
+# = End IK =
+
+# == End Model ==
 
 """
 === REVIEW 3 , TUTORIAL 2 =====================================================================================
@@ -231,8 +344,66 @@ f_Bi  = dot( I_i , a_i ) + cross_star( v_i ,                          # ^-- This
 f_Ji  = f_Bi + \sum_{j \in c(i)}( f_Jj ) # Calculate the joint forces from the body forces
 tau_i = dot( transpose( s_i ) , f_Ji ) # Joint torque
 
-FIXME : PAGE 6
 
+~~ Kinematics ~~
+
+Body positions can be calculated recursively using the formula:
+    
+X_{i->0} = np.dot( X_{i->p(i)} , X_{p(i)->0} ) , p(i) != 0
+
+which calculates the coordinate transform from body coordinate frame i to the reference frame (0) 
+
+If frame b is already close to frame d , then we can use an approximated small screw displacement to move frame b to be coincident with d
+
+pDelta = XtoV( X_{d->b} ) = XtoV( dot( X_{d->0} , X_{0->b} ) )
+
+If frame F_d is a reachable position for body b , then there will be a joint position change , qDelta , that causes a displacement pDelta
+in body b. In general, qDelta will not be unique, therefore the relationship between pDelta and qDelta is not closed form and may be 
+difficult to obtain. However , this relationship can be approximated:
+
+pDelta = dot( J_{b->b} , qDelta )
+
+J_{b->b} : Jacobian for body b expressed in b coordinates
+
+~~ Jacobians ~~
+
+A jacobian is a matrix that maps the joint-space velocity vector qDot to some other kind of velocity.
+In this tutorial, the body Jacobian is the matrix that maps qDot to the spatial velocity of body b.
+
+v_b = dot( J_b , qDot )
+
+The velocity of the body can be expressed as 
+
+v_b = \sum_{i}{body->root}( dot( s_i , qDot_i ) )
+
+which states that v_b is the sum of the joint velocities of all the joints on the path between the body and the fixed base.
+This can be rewritten as
+                                                                      / 1 , if i in the chain between the body and the root link
+v_b = \sum_{i=1}{N}( e_{bi} * dot( s_i , qDot_i ) ) , where e_{bi} = {
+                                                                      \ 0 , otherwise
+                                                                      
+yielding the following expression
+
+J_b = [ e_{b1} * s_1 , e_{b2} * s_2 , ... , e_{bi} * s_i , ... , e_{bN} * s_N ]
+
+So the body jacobian is a 6 x N matrix whose ith column is either s_i or [0] , depending on whether or not joint i is on the path to the root
+
+More generally, a body jacobian is a 1 x N block matrix in which the ith block is a 6 x n_i matrix e_{bi} * s_i
+Every column of the body jacobian must be expressed in the same coordinate system as the velocity vector that it maps to.  The coordinate
+transformation rule for a body is
+
+J_{B->b} = dot( X_{B->A} , J_{A->b}
+
+~ A Common Trap ~
+
+Consider an equation of the form
+
+[ [ w ] , = dot( [ J ], [ qDot ] ) 
+  [ v ] ]
+Where w and v are described as the angular and linear velocity of some link expressed in the lab frame coordinates.
+
+In translating this START HERE
+FIXME : PAGE 8
 """
 
 """
