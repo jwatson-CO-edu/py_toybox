861b52e8bc776260730b3fd11f42cbd31104fd64
jwatson_utah_edu <james.r.watson@utah.edu>
2017-09-20 22:46:28 -0600

    I screwed everything up somehow

--- VSM_sketch_01_QLearning.py
+++ VSM_sketch_01_QLearning.py
@@ -173,31 +173,6 @@ class HeartRate: # <<< MH
             time.sleep( self.period - elapsed )
         self.last = time.time()
 
-class RollingList( list ): # << MH
-    """ A rolling window based on 'list' """ 
-    
-    def __init__( self , winLen , *args ):
-        """ Normal 'list' init """
-        list.__init__( self , [ 0 ] * winLen , *args )
-        
-    def append( self , item ):
-        """ Append an item to the back of the list """
-        list.append( self , item )
-        del self[0]
-        
-    def prepend( self , item ):
-        """ Prepend an item to the front of the list """
-        self.insert( 0 , item )
-        del self[-1]
-        
-    def get_average( self ):
-        """ Get the rolling average , NOTE: Calling this function after inserting non-numeric data will result in an error """
-        return sum( self ) * 1.0 / len( self )
-        
-    def item_list( self ):
-        """ Return a copy of the RollingList as a list """
-        return self[:]
-
 # == End Utility ==
 
 
