9517899aecc71bf8d7a6ac0b8d0a67261d0dab30
James Watson <james.r.watson@utah.edu>
2018-05-11 15:48:35 -0600

    Initial SVD investigation , MARCHHARE enlisted for vector math

--- SVD_and_Pseudoinverse.py
+++ SVD_and_Pseudoinverse.py
@@ -16,8 +16,10 @@ __progname__ = "PROGRAM NAME"
 __version__  = "YYYY.MM.DD"
 """  
 ~~~ Developmnent Plan ~~~
-[ ] Generate points in a parallelepiped
+[Y] Generate points in a parallelepiped
 [ ] Investigate SVD of points
+    [ ] Get SVD of sampled points
+    [ ] Test if the Right Singular Vectors are parallel to each other
 [ ] Investigate Pseudoinverse of points
 """
 
@@ -28,9 +30,11 @@ SOURCEDIR = os.path.dirname( os.path.abspath( __file__ ) ) # URL, dir containing
 # ~~~ Imports ~~~
 # ~~ Standard ~~
 from math import pi , sqrt
+from random import random
 # ~~ Special ~~
 import numpy as np
 # ~~ Local ~~
+from marchhare.Vector import vec_unit , vec_mag
 
 # ~~ Constants , Shortcuts , Aliases ~~
 EPSILON = 1e-7
@@ -51,15 +55,19 @@ def sample_in_hyperparallelepiped( N , corner , magBases ):
     """ Sample 'N' uniformly-distributed points in the 'hyperparallelepiped' with 'corner' and bases 'magBases' """
     # NOTE: 'magBases' also specifies the maximum extent beyond the corner for each dimension
     # NOTE: It is possible for the number of bases to differ from the number of dimensions
-    
-    # FIXME : START HERE!
-    
-    # 1. Enforce consisistent dimensionality
-    # 2. For each i
-    # 3. Start at corner
-    # 4. For each dimension basis
-    # 5. Sample along
-
+    unitBases = [ vec_unit( base ) for base in magBases ]
+    baseMags  = [ vec_mag( base )  for base in magBases ]
+    rtnSamples = []
+    # 1. For each i
+    for i in xrange( N ):
+        sample = corner[:]
+        # 2. For each dimension basis
+        for baseDex , base in enumerate( unitBases ):
+            # 3. Sample along each of the bases  &&  4. Add all the sample parts to the corner and add to the list
+            sample = np.add( sample , 
+                             np.multiply( base , baseMags[ baseDex ] * random() ) )
+        rtnSamples.append( sample )
+    return rtnSamples
 # _ End Func _
 
 
@@ -73,6 +81,18 @@ if __name__ == "__main__":
     print __prog_signature__()
     termArgs = sys.argv[1:] # Terminal arguments , if they exist
     
+    #cuboidSamples = sample_in_hyperparallelepiped( 10 , [ 0 , 0, 0 , 0 ] , [ [10,0,0,0] , [0,10,0,0] , [0,0,10,0] ] )
+    #print cuboidSamples
+    
+    prllrppdSamples = sample_in_hyperparallelepiped( 1000 , 
+                                                     [  0 ,  0 ,  0 ,  0 ] , 
+                                                     [ [  2 ,  2 , 40 ,  0 ] , [ -2 , 20 , -2 , 0] , [ 10 , -2 , -2 , 0 ] ] )
+    a = np.matrix( prllrppdSamples )
+    u , s , vt = np.linalg.svd( a , full_matrices = True )
+    v = np.transpose( vt )
+    print "S = " , s , endl , "S , The influence of each Right Singular Vector in decreasing order"
+    print "V = " , endl , v , endl , "V , Right Singular (column) Vectors, correspinding to the singular values S"
+    # Note that the 
 
 # ___ End Main _____________________________________________________________________________________________________________________________
 
