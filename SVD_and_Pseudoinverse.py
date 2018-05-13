04d0842d6ec0f72c7442d2c56d33ad2ae4af4e65
James Watson <james.r.watson@utah.edu>
2018-05-12 19:04:19 -0600

    Angle between singular vectors

--- SVD_and_Pseudoinverse.py
+++ SVD_and_Pseudoinverse.py
@@ -5,20 +5,20 @@
 from __future__ import division # Future imports must be called before everything else, including triple-quote docs!
 
 """
-FILENAME.py , Built on Wing 101 IDE for Python 2.7
-James Watson, YYYY MONTHNAME , Template Version: 2018-01-08
-A ONE LINE DESCRIPTION OF THE FILE
+SVD_and_Pseudoinverse.py , Built on Wing 101 IDE for Python 2.7
+James Watson, 2018 May , Template Version: 2018-01-08
+Singular Value Decomposition and its geometric meaning
 URL: https://www.johndcook.com/blog/2018/05/05/svd/
 
 Dependencies: numpy
 """
-__progname__ = "PROGRAM NAME"
+__progname__ = "Singular Value Decomposition"
 __version__  = "YYYY.MM.DD"
 """  
 ~~~ Developmnent Plan ~~~
 [Y] Generate points in a parallelepiped
 [ ] Investigate SVD of points
-    [ ] Get SVD of sampled points
+    [Y] Get SVD of sampled points
     [ ] Test if the Right Singular Vectors are parallel to each other
 [ ] Investigate Pseudoinverse of points
 """
@@ -34,7 +34,7 @@ from random import random
 # ~~ Special ~~
 import numpy as np
 # ~~ Local ~~
-from marchhare.Vector import vec_unit , vec_mag
+from marchhare.Vector import vec_unit , vec_mag , vec_angle_between
 
 # ~~ Constants , Shortcuts , Aliases ~~
 EPSILON = 1e-7
@@ -92,7 +92,13 @@ if __name__ == "__main__":
     v = np.transpose( vt )
     print "S = " , s , endl , "S , The influence of each Right Singular Vector in decreasing order"
     print "V = " , endl , v , endl , "V , Right Singular (column) Vectors, correspinding to the singular values S"
-    # Note that the 
+    # Angles between Right Singular Vectors
+    v1 = np.ravel( v[0:3,0].reshape(3,1) ) 
+    v2 = np.ravel( v[0:3,1].reshape(3,1) )
+    v3 = np.ravel( v[0:3,2].reshape(3,1) )
+    print "Angle between Column 1 and 2" , vec_angle_between( v1 , v2 ) / pi , "pi radians"
+    print "Angle between Column 2 and 3" , vec_angle_between( v2 , v3 ) / pi , "pi radians"
+    print "Angle between Column 3 and 1" , vec_angle_between( v3 , v1 ) / pi , "pi radians"
 
 # ___ End Main _____________________________________________________________________________________________________________________________
 
