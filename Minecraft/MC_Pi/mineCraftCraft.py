ddaa7037bfcfc262e2572b67c77462e9b060443f
jwatson_utah_edu <james.r.watson@utah.edu>
2019-02-06 16:19:13 -0700

    Adding MARCHHARE to manip minecraft

---
+++ Minecraft/MC_Pi/mineCraftCraft.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Template Version: 2018-03-23
+
+# ~~ Future First ~~
+from __future__ import division # Future imports must be called before everything else, including triple-quote docs!
+
+"""
+mineCraftCraft.py
+James Watson, 2019 February
+Utilities for working with Minecraft Pi Edition
+"""
+
+# === ARCHITECTURE =========================================================================================================================
+
+# URL , L-System: https://en.wikipedia.org/wiki/L-system
+
+
+
+# ___ END ARCH _____________________________________________________________________________________________________________________________
+
+
+# === Testing ==============================================================================================================================
+
+if __name__ == "__main__":
+    pass
+
+# ___ End Tests ____________________________________________________________________________________________________________________________
