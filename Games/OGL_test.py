29f50dc3cba5088187a26ea9e28b3e5bf4e38547
James Watson <jawa5023@colorado.edu>
2019-08-24 23:38:03 -0600

    Demo runs but cannot see geometry

--- Games/OGL_test.py
+++ Games/OGL_test.py
@@ -1,33 +1,28 @@
-import marchhare.OGL_Shapes
-
 #!/usr/bin/env python3
-# show_spiral.py
+
+
+
 # James Watson , 2019 August
-# Show a spiral insertion operation
+
 
 import os , sys , time
+SOURCEDIR = os.path.dirname( os.path.abspath( '__file__' ) ) # URL, dir containing source file: http://stackoverflow.com/a/7783326
+PARENTDIR = os.path.dirname( SOURCEDIR )
+# ~~ Path Utilities ~~
+def prepend_dir_to_path( pathName ): sys.path.insert( 0 , pathName ) # Might need this to fetch a lib in a parent directory
+prepend_dir_to_path( PARENTDIR )
 
 from random import randrange
 from math import pi
 import numpy as np
-from OGL_Shapes import OGL_App , Point_OGL , CartAxes , Vector_OGL , Trace_OGL
+from marchhare.OGL_Shapes import OGL_App , Point_OGL , CartAxes , Vector_OGL , Trace_OGL , CameraOrbit
 from pyglet.window import key
-
-from dataProcess import *
-from math_kit import vec_sphr
-
-libPath = os.path.join( os.path.expanduser( '~' ) , 'dev_rmstudio/lib/' )
-print( libPath )
-
-sys.path.insert( 0 , libPath )
 
 _DEBUG = False
 
 # ~~ Render Data ~~
 
 objs = []
-
-_SHOWTRACE = 1
 
 cam = CameraOrbit()
 
@@ -36,56 +31,36 @@ forceVc = Vector_OGL()
 torqVec = Vector_OGL()
 torqVec.color = ( 200 , 200 , 200 )
 
-if _SHOWTRACE:
-    trace = Trace_OGL()
-    trace.set_color( (255,255,255,) )
-    for pnt in positions:
-        trace.add_point( pnt )
-        if _DEBUG: print( "Adding:" , pnt )
-    objs.append( trace )
-    center = positions[-1]
-    offset = 0.005
-    cam.center = center 
-    cam.r      = offset
-    cam.dR     = 0.0005
-    nearClip   = 0.001
-    farClip    = 0.750
-    
-    objs.append( tcpAxes )
-    objs.append( forceVc )
-    objs.append( torqVec )
-    
-else:
-    for x in [0,1]:
-        for y in [0,1]:
-            for z in [0,1]:
-                objs.append(
-                    Point_OGL( pnt = [ x , y , z ] )
-                )
+for x in [0,1]:
+    for y in [0,1]:
+        for z in [0,1]:
+            objs.append(
+                Point_OGL( pnt = [ x , y , z ] )
+            )
 
-    objs.append( CartAxes() )
+objs.append( CartAxes() )
 
-    vec = Vector_OGL()
-    vec.set_origin_displace( [0,1,0] , [0,0,1] )
-    vec.set_color( (randrange(0,256),randrange(0,256),randrange(0,256),) )
-    objs.append( vec )
+vec = Vector_OGL()
+vec.set_origin_displace( [0,1,0] , [0,0,1] )
+vec.set_color( (randrange(0,256),randrange(0,256),randrange(0,256),) )
+objs.append( vec )
 
-    trace = Trace_OGL()
-    trace.set_color( (255,255,255,) )
-    trace.add_point( [0,0,0] )
-    trace.add_point( [1,0,0] )
-    trace.add_point( [1,1,0] )
-    trace.add_point( [0,1,0] )
-    trace.add_point( [0,0,1] )
-    objs.append( trace )
+trace = Trace_OGL()
+trace.set_color( (255,255,255,) )
+trace.add_point( [0,0,0] )
+trace.add_point( [1,0,0] )
+trace.add_point( [1,1,0] )
+trace.add_point( [0,1,0] )
+trace.add_point( [0,0,1] )
+objs.append( trace )
     
-    center = [0,0,0]
-    offset = 0.75
-    cam.center = center 
-    cam.r      = offset
-    cam.dR     = 0.050
-    nearClip   = 0.001
-    farClip    = 0.100
+center = [0,0,0]
+offset = 0.75
+cam.center = center 
+cam.r      = offset
+cam.dR     = 0.050
+nearClip   = 0.001
+farClip    = 0.100
 
 
 window = OGL_App( objs , # ------------------------- List of objects to render
@@ -95,9 +70,6 @@ window = OGL_App( objs , # ------------------------- List of objects to render
 
 window.set_camera( *cam.get_cam_vectors() )
 window.set_view_params( 75 , nearClip , farClip )
-
-
-
 
 @window.event
 def on_key_press( symbol , modifiers ):
@@ -157,20 +129,6 @@ while not window.has_exit:
     if _DEBUG: window.prnt_view_params()
     
     # B. Animate Axes
-    if _SHOWTRACE:
-        index = i % len( posSeries )
-        i += 1
-        tcpXform = poselib.pose_vec_to_mtrx( posSeries[ index ] )
-        tcpAxes.xform = tcpXform
-        forceVc.xform = tcpXform
-        torqVec.xform = tcpXform
-        
-        # C. Animate forces
-        forceVc.set_origin_displace( [0,0,0] ,
-                                     np.multiply( forces[ index ] , 0.0001 ) )
-        torqVec.set_origin_displace( [0,0,0] ,
-                                     np.multiply( torques[ index ] , 0.0005 ) )
-        
     
     # D. Redraw
     window.dispatch_events() # Handle window events
