4cb2fe85e78618e6a65276355e39f232d22258c5
James Watson <james.watson-2@colorado.edu>
2019-09-01 12:25:07 -0600

    chevrons

--- chevrons.py
+++ chevrons.py
@@ -5,8 +5,15 @@
 
 import random
 
+seq = [ 
+    [ '\\' , '/' ] ,
+    [ '\\' , '/' , '-' , '|' , '_' , ' ' , ' ' ] ,
+    [ '\\' , '/' , ' ' ] ,
+    [ '\\' , '/' , '_' ] ,
+]
+
 for i in range( 100 ):
     row = ""
     for j in range( 100 ):
-        row += random.choice( [ '\\' , '/' , '-' , '|' , '_' , ' ' , ' ' ] )
+        row += random.choice( seq[3] )
     print( row )
