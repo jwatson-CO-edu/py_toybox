10a58e547038ed3b6dff5dff31127d4fe3a0ab89
James Watson <james.watson-2@colorado.edu>
2020-11-13 00:31:46 -0700

    Added a sprite base class

--- vpython/ships_00.py
+++ vpython/ships_00.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 # Template Version: 2020-02-07
 
-__progname__ = "PROGRAM_NAME.py"
+__progname__ = "ships_00.py"
 __author__   = "James Watson"
-__version__  = "YYYY.MM"
-__desc__     = "A_ONE_LINE_DESCRIPTION_OF_THE_FILE"
+__version__  = "2020.11"
+__desc__     = "Display and control of 3D sprites in vpython"
 
 """  
 ~~~ DEV PLAN ~~~
-[ ] ITEM1
+[ ] Adapt `Stinger` to inherit `VP_CompositeGeo`
 [ ] ITEM2
 """
 
@@ -26,6 +26,7 @@ sys.path.insert( 0 , PARENTDIR ) # Might need this to fetch a lib in a parent di
 # ~~~ Imports ~~~
 # ~~ Standard ~~
 # ~~ Special ~~
+import vpython 
 from vpython import *
 from numpy import sqrt, cos, sin, pi
 import numpy as np
@@ -33,7 +34,6 @@ from marchhare.Vector import vec_unit
 # ~~ Local ~~
 #import marchhare
 #import marchhare.VectorMath.HomogXforms
-
 from marchhare.VectorMath.HomogXforms import set_position , pose_from_position_bases , position_bases_from_pose
 
 
@@ -67,11 +67,65 @@ def vp_bases_from_np_homog( homogPose ):
 
 ########## Classes ################################################################################
 
-class Origin:
+class VP_Kit:
+    clr_lgt_blue = vec( 69/255, 118/255, 255/255 )
+
+
+class VP_Sprite( vpython.compound ):
+    """ vpython drawable """
+    
+    def __init__( self , name = "VP_Object" ):
+        """ Init geometry """
+        super().__init__( self.get_geometry() )
+        self.name = name    
+        self.pose = self.get_geo_pose()
+    
+    def get_geometry( self ):
+        """ Get the drawable geometry """
+        return [ box() ]
+    
+    def get_geo_pose( self ):
+        """ Get the geo translation/orientation vectors and convert them into a homogeneous pose """
+        # Assume that the `axis` is Z and the `up` is Y. That is, forward is +Z in the ship's own frame
+        #print( self.geo.axis , self.geo.up )
+        zBasis = vec_unit( vp_vec_to_np_arr( self.axis ) )
+        yBasis = vec_unit( vp_vec_to_np_arr( self.up ) )
+        xBasis = np.cross( yBasis , zBasis )
+        posn   = vp_vec_to_np_arr( self.pos )
+        return pose_from_position_bases( posn , xBasis , yBasis , zBasis )    
+    
+    
+class Origin( VP_Sprite ):
     """ Graphical representation of pose bases """
     
-    def __init__( self , homogPose , scale ):
-        pass
+    def __init__( self , homogPose = np.eye(4) , scale = 1.0 ):
+        """ Set size and fetch geo """
+        self.scale = scale
+        super().__init__( name = "Origin" )
+    
+    def get_geometry( self ):
+        """ Get the drawable geometry """
+        return [
+            arrow(
+                pos        = vector( 0.0        , 0.0 , 0.0 ) ,
+                axis       = vector( self.scale , 0.0 , 0.0 ) ,
+                color      = vector( 1.0        , 0.0 , 0.0 ) ,
+                shaftwidth = self.scale/10.0
+            ) ,
+            arrow(
+                pos        = vector( 0.0 , 0.0        , 0.0 ) ,
+                axis       = vector( 0.0 , self.scale , 0.0 ) ,
+                color      = vector( 0.0 , 1.0        , 0.0 ) ,
+                shaftwidth = self.scale/10.0
+            ) ,
+            arrow(
+                pos        = vector( 0.0 , 0.0 , 0.0        ) ,
+                axis       = vector( 0.0 , 0.0 , self.scale ) ,
+                color      = VP_Kit.clr_lgt_blue              ,
+                shaftwidth = self.scale/10.0
+            ) ,              
+        ]    
+
 
 class Starship:
     """ Represents a star-craft in a simulation """
@@ -137,12 +191,14 @@ class Stinger( Starship ):
 sqrt2 = sqrt(2)
 
 shipGreen  = vec( 0.0 , 1.0 , 0.0 ) #vector(106/255, 196/255, 124/255)
-shipBlue   = vec( 69/255, 118/255, 255/255 ) #vector(106/255, 124/255 , 196/255)
+shipBlue   = VP_Kit.clr_lgt_blue #vector(106/255, 124/255 , 196/255)
 shipOrange = vec( 1.0 , 0.0 , 0.0 ) #vector(255/255, 148/255, 54/255)
 BGcolor    = vec( 0.0 , 0.0 , 0.0 ) #vec(48/255, 8/255, 8/255)
 trailColor = vec( 1.0 , 1.0 , 0.0 ) #vector( 255/255, 244/255, 25/255 )
 
-if __name__ == "__main__" and 0:
+#print( type( compound([box()]) ) )
+
+if __name__ == "__main__" and 1:
     print( __progname__  , 'by' , __author__ , ', Version:' , __version__ )
     termArgs = sys.argv[1:] # Terminal arguments , if they exist
 
@@ -160,8 +216,11 @@ if __name__ == "__main__" and 0:
     ship = Stinger()
     
     ship.geo.rotate( angle = -pi/2.0 , axis=vector(1.0,0.0,0.0) )
+    ship.geo.rotate( angle = -pi     , axis=vector(0.0,1.0,0.0) )
     
     ship.load_geo_pose()
+    
+    orgn = Origin( scale = 20.0 )
     
     while 1:
         rate( FPS )
