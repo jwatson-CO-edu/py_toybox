01c4612c22b40655809f008ff285ddbaf59fcf1e
James Watson <james.watson-2@colorado.edu>
2021-11-17 17:35:59 -0700

    editorial changes

--- Geometric/Clifford/00_geo_product.py
+++ Geometric/Clifford/00_geo_product.py
@@ -1,5 +1,10 @@
+########## INIT ###################################################################################
 import numpy as np
 from scipy.special import comb
+
+
+
+########## UTIL FUNCTIONS #########################################################################
 
 def count_combos( dim ):
     """ Count the number of combinations we can pick from dim """
@@ -17,44 +22,24 @@ def accum_elems( numLst ):
         rtnLst.append( total )
     return rtnLst
 
+
+
+########## Composite ##############################################################################
+
 class Composite:
-    """ Represents a CLifford object [ e1, e2, e3, ..., e12, e13, e23, ..., e123, ... ] """
-    def __init__( self, dim = 3 ):
-        self.partLims = count_combos( dim )
+    """ Represents a Clifford object [ e1, e2, e3, ..., e12, e13, e23, ..., e123, ... ] """
+    
+    def __init__( self, realDim = 3 ):
+        """ Build a list to hold all the parts of a clifford composite """
+        self.partLims = count_combos( realDim )
         self.parts    = [None for i in range( sum( self.partLims ) )]
         self.partLims = accum_elems( self.partLims )
 
 
-class VectorR2:
+##### Composite Operations #####
+
+def wedge( cmp1, cmp2 ):
     # FIXME: START HERE
+    # FIXME: WEDGE VECTORS
+    # FIXME: WEDGE HIGHER VECTORS
     pass
-
-class BivectorR2:
-
-    def __init__( self, vec1, vec2, mag ):
-        self.vec1 = vec1
-        self.vec2 = vec2
-        self.mag  = mag
-
-    def wedge( self, other ):
-        # FIXME: START HERE
-        pass
-
-class CompositeR2:
-    """ Represent the general geometric object 
-    https://www.youtube.com/watch?v=PNlgMPzj-7Q&list=PLpzmRsG7u_gqaTo_vEseQ7U8KFvtiJY4K&index=1 """
-    
-    e1 = np.array( [ 1.0, 0.0 ] )
-    e2 = np.array( [ 0.0, 1.0 ] )
-
-    def __init__( self, value = [ 1.0, 0.0 ] ):
-        self.val = value
-
-    def as_np( self ):
-        return np.array( self.val )
-
-    def dot( self, other ):
-        return np.dot( self.as_np(), other.as_np() )
-
-    def wedge( self, other ):
-        pass
