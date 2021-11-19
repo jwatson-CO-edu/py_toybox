5ddb313d29723921587d9a180ccfe4e0837f7f31
James Watson <james.watson-2@colorado.edu>
2021-11-18 20:45:21 -0700

    add representation

--- Geometric/Clifford/00_geo_product.py
+++ Geometric/Clifford/00_geo_product.py
@@ -26,14 +26,30 @@ def accum_elems( numLst ):
 
 ########## Composite ##############################################################################
 
-class Composite:
-    """ Represents a Clifford object [ e1, e2, e3, ..., e12, e13, e23, ..., e123, ... ] """
+class Mvec:
+    """ Represents a Clifford multivector [ e1, e2, e3, ..., e12, e13, e23, ..., e123, ... ] """
     
     def __init__( self, realDim = 3 ):
         """ Build a list to hold all the parts of a clifford composite """
+        self.rDim     = realDim
         self.partLims = count_combos( realDim )
-        self.parts    = [None for i in range( sum( self.partLims ) )]
+        self.blades   = [None for i in range( sum( self.partLims ) )]
         self.partLims = accum_elems( self.partLims )
+        self.bladNams = self.get_blade_labels()
+
+    def get_blade_labels( self ):
+        """  Create labels e1, e2, e3, ... """
+        dimInt = [ i+1 for i in range( self.rDim ) ]
+        return [ 'e'+str( elem ) for elem in dimInt ]
+
+    def __repr__( self ):
+        """ Print the multivector """
+        rtnStr = '[ '
+        for i in range( self.rDim ):
+            rtnStr += str( self.blades[i] ) + '*' + self.bladNams[i] + ', '
+        rtnStr += ']'
+        return rtnStr
+
 
 
 ##### Composite Operations #####
